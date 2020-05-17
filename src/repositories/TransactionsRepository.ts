baec6c8c0f49be86cc1ce812fdab66d6b33c755d
elascalla <erick.lascalla@gmail.com>
2020-05-17 01:17:20 -0400

    Adjusts in first project with node

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,17 @@ interface Balance {
   total: number;
 }
 
+interface TransactionsBalance {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
+interface TransactionsDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +24,47 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): TransactionsBalance {
+    const transactionsBalance = {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
+
+    return transactionsBalance;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (acc: Balance, cur: Transaction) => {
+        if (cur.type === 'income') {
+          acc.income += cur.value;
+        } else {
+          acc.outcome += cur.value;
+        }
+
+        acc.total = acc.income - acc.outcome;
+
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionsDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+
+  public findTransactionByTitle(title: string) {
+    return this.transactions.find(transaction => transaction.title === title);
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction | null {
+    const findTransaction = this.transactionsRepository.findTransactionByTitle(
+      title,
+    );
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error("Type is 'income' or 'outcome'");
+    }
+
+    if (Math.sign(value) < 0) {
+      throw Error('Please enter value positive!');
+    }
+
+    if (findTransaction) {
+      throw Error('Exists one transaction with the same title');
+    }
+
+    return this.transactionsRepository.create({ title, value, type });
   }
 }
 
